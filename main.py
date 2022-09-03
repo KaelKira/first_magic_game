6327da710acf11d723ba8c38d8287716e2ad18f1
Aleksandr Matveev <amatveev@avito.ru>
2022-09-03 12:10:53 +0300

    first-commit

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+class Сharacter:
+    """базовый класс персонажа"""
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name, attack, accuracy, dodge, hp):
+        """Constructor"""
+        self.name = name
+        self.attack = attack
+        self.accuracy = accuracy
+        self.dodge = dodge
+        self.hp = hp
+
+    def say_name(self):
+        print(self.name)
+
+    def info(self):
+        print('Класс ' + self.name)
+        print("Атака: %s" % self.attack)
+        print("Меткость: %s" % self.accuracy)
+        print("Уклонение: %s" % self.dodge)
+        print("HP: %s" % self.hp)
+
+    def attacking(self):
+        return self.attack
+
+    def dodging(self):
+        return self.dodge
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+if __name__ == "__main__":ч
+    war = Сharacter("War", 5, 4, 2, 10)
+    mag = Сharacter("Mag", 2, 10, 2, 8)
+    archer = Сharacter("Archer", 7, 3, 5, 10)
+    print('Приветствую! Это игра матч-арена фэнтезийных персонажей. Собери самый эффективный отряд из трёх персонажей '
+          'и вступи в битву!')
+    print('Доступны следующие персонажи:')
+    war.say_name()
+    mag.say_name()
+    archer.say_name()
+
+# тут будет код выбора персонажей
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
